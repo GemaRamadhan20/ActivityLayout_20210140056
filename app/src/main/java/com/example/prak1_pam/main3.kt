634714ca3fd9602d000924d436f662c1bdac6337
Gema Ramadhan <gema.ramadhan.ft21@mail.umy.ac.id>
2023-10-04 14:15:28 +0700

    menambahkan text Toyota Supra 1994 dan Logo Panah

---
+++ app/src/main/java/com/example/prak1_pam/main3.kt
@@ -0,0 +1,11 @@
+package com.example.prak1_pam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class main3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main3)
+    }
+}
