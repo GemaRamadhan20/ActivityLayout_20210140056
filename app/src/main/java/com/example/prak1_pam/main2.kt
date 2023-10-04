fd64a9dfd3213312eaa70c1bb56d33a139b1673c
Gema Ramadhan <gema.ramadhan.ft21@mail.umy.ac.id>
2023-10-04 09:47:20 +0700

    Menambahkan Gambar Mobil dan BUTTON

---
+++ app/src/main/java/com/example/prak1_pam/main2.kt
@@ -0,0 +1,11 @@
+package com.example.prak1_pam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class main2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
